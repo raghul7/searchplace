1815ad38f1cdd6cd51fddf1b4db5e23b1677bfea
Sk <raghul.sk@capgemini.com>
2022-10-18 19:30:59 +0530

    google key

--- src/Component/Map/index.jsx
+++ src/Component/Map/index.jsx
@@ -4,7 +4,7 @@ import './style.scss';
  const Map=({searchCountryName})=>{
     return (
         <div className="google-map-code">
-          <iframe src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAKJdprYRNzrXMGlfn_NnscD_c1ZfMGtgE&q=${searchCountryName}`} width="1000" height="500" padding="20px" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+          <iframe src={`https://www.google.com/maps/embed/v1/place?key=[googleMapKey]&q=${searchCountryName}`} width="1000" height="500" padding="20px" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
         </div>
     );
  }
